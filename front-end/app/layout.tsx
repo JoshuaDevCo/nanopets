24070db3bfc9b19f265df6fffa7b97025653813a
elonsdev.eth <83188557+elonsdev@users.noreply.github.com>
2024-10-31 16:31:10 +0200

    add telegram script in head

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Micro_5 } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const micro = Micro_5({ subsets: ["latin"], weight: ["400"], display: "swap" });
 
@@ -16,6 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <Script
+          src='https://telegram.org/js/telegram-web-app/js'
+          strategy='beforeInteractive'
+        />
+      </head>
       <body className={`${micro.className} flex justify-center bg-blue-50`}>
         {children}
       </body>
