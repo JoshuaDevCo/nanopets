8f5665c5e076daf2ab79daec78ef88b91275edeb
elonsdev.eth <83188557+elonsdev@users.noreply.github.com>
2024-10-31 17:33:08 +0200

    update cors

--- back-end/index.js
+++ back-end/index.js
@@ -15,8 +15,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000" || "https://nanopets.vercel.app",
-    methods: ["GET", "POST"],
+    origin: ["http://localhost:3000", "https://nanopets.vercel.app"],
+    methods: ["GET", "POST", "OPTIONS"],
   },
 });
 
