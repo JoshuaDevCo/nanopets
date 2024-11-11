8d073fd7a8fb4c4d7e134633b090318172e01927
elonsdev.eth <83188557+elonsdev@users.noreply.github.com>
2024-11-11 11:44:44 +0200

    actually fix ai responding every time view changes.

--- front-end/app/game/useTamagotchiGame.ts
+++ front-end/app/game/useTamagotchiGame.ts
@@ -274,6 +274,7 @@ export function useTamagotchiGame() {
         triggerAnimation(Coin, 10);
       }
       setLastAction(action);
+      setTimeout(() => setLastAction(null), 1000);
       setError(null);
       setIsBusyAction(false);
     } catch (error: any) {
@@ -295,6 +296,7 @@ export function useTamagotchiGame() {
       setTamagotchi(response.data);
       setClockTime(response.data.clockTime);
       setLastAction(`purchase_${itemId}`);
+      setTimeout(() => setLastAction(null), 1000);
       setError(null);
       setIsBusyAction(false);
     } catch (error: any) {
