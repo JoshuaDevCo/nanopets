98360e991cb2af70871f31f2056917da9ef07783
elonsdev.eth <83188557+elonsdev@users.noreply.github.com>
2024-11-11 11:48:24 +0200

    fix ai. not displaying on view change now :)

--- front-end/app/game/page.tsx
+++ front-end/app/game/page.tsx
@@ -260,7 +260,7 @@ export default function TamagotchiGame() {
                     alt='pet'
                     width={300}
                     height={300}
-                    className={`hue-rotate-[${tamagotchi.tamahue}deg]`}
+                    className={`hue-rotate-[${tamagotchi.tamahue!}deg]`}
                   />
                   {tamagotchi.isSleeping && (
                     <Image
